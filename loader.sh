1bf02c4d6074cd8fff11b9cb0dac2c7312114655
Simon You <youyi@shanghaitech.edu.cn>
2016-03-16 03:26:47 +0800

    goodmorning

--- loader.sh
+++ loader.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir /usr/src/wordpress/wp-content/themes/himalayas
-cp -ri /cnt/himalayas/* /var/www/html/wp-content/themes/himalayas/
+cp -R /cnt/himalayas/. /var/www/html/wp-content/themes/himalayas/
 mkdir /usr/src/wordpress/wp-content/uploads
-cp -ri /cnt/uploads/* /var/www/html/wp-content/uploads/
+cp -R /cnt/uploads/. /var/www/html/wp-content/uploads/
 
 
 /entrypoint.sh apache2-foreground
