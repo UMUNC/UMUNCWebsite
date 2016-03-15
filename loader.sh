73e09123699ac1305bc0ac9c0f8e13db75e9d04c
Simon You <youyi@shanghaitech.edu.cn>
2016-03-16 02:45:07 +0800

    hi

--- loader.sh
+++ loader.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+/entrypoint.sh apache2-foreground
 
 mkdir /var/www/html/wp-content/themes/himalayas
 cp -ri /cnt/himalayas/* /var/www/html/wp-content/themes/himalayas/
 mkdir /var/www/html/wp-content/uploads
 cp -ri /cnt/uploads/* /var/www/html/wp-content/uploads/
 
-/entrypoint.sh apache2-foreground
