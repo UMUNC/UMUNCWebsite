750af837313a2b75d5b5bc3f9553bdc07d80fe13
Simon You <youyi@shanghaitech.edu.cn>
2016-03-16 02:13:57 +0800

    hi

---
+++ loader.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir /var/www/html/wp-content/themes/himalayas
+cp -ri /cnt/himalayas/* /var/www/html/wp-content/themes/himalayas/
+mkdir /var/www/html/wp-content/uploads
+cp -ri /cnt/uploads/* /var/www/html/wp-content/uploads/
+
+apache2-foreground
