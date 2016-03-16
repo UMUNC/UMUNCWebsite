b9b7ae5f4e39df81b7ce7b026fcf96506e63fbe5
Simon You <youyi@shanghaitech.edu.cn>
2016-03-16 23:59:44 +0800

    hi

--- loader.sh
+++ loader.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 mkdir /usr/src/wordpress/wp-content/themes/himalayas
 cp -R /cnt/himalayas/. /usr/src/wordpress/wp-content/themes/himalayas/
+chown -R www-data:www-data /usr/src/wordpress/wp-content/themes/himalayas/
 mkdir /usr/src/wordpress/wp-content/uploads
 cp -R /cnt/uploads/. /usr/src/wordpress/wp-content/uploads/
+chown -R www-data:www-data /usr/src/wordpress/wp-content/uploads/
 
 
 /entrypoint.sh apache2-foreground
-
